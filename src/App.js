3be2cd39a9ac67b30352a2c45c27dda3d277b20f
Pooja <poojapatidarpatidar10711@gmail.com>
2023-04-21 16:02:21 +0530

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
         
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        jayshreeee
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
